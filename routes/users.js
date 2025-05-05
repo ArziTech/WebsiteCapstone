b11610981f5dec942b13910b3f3846e97e2a9b3f
ArziTech <wawcam123@gmail.com>
2025-05-05 22:14:50 +0700

    connect with db

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  // Contoh route untuk mendapatkan data dari database
+  try {
+    const { rows } = await pool.query('SELECT * FROM users');
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
