d19aad63e3f0bc24114bcda5846844211d00030e
ArziTech <wawcam123@gmail.com>
2025-05-06 09:39:22 +0700

    device routes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const { Pool } = require('pg');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const devicesRouter = require('./routes/devices');
+const connectDB = require('./lib/db');
 
 const app = express();
 
@@ -21,23 +23,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/devices', devicesRouter);
 
-const pool = new Pool({
-  user: 'capstone',
-  host: 'localhost',
-  database: 'capstone',
-  password: 'admin',
-  port: 5432,
-});
 
-// Test koneksi database
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('Error connecting to PostgreSQL:', err);
-  } else {
-    console.log('Connected to PostgreSQL at:', res.rows[0].now);
-  }
-});
+
+// const pool = new Pool({
+//   user: 'capstone',
+//   host: 'localhost',
+//   database: 'capstone',
+//   password: 'admin',
+//   port: 5432,
+// });
+
+// // Test koneksi database
+// pool.query('SELECT NOW()', (err, res) => {
+//   if (err) {
+//     console.error('Error connecting to PostgreSQL:', err);
+//   } else {
+//     console.log('Connected to PostgreSQL at:', res.rows[0].now);
+//   }
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -2,25 +2,33 @@ const express = require('express');
 const {Pool} = require("pg");
 const router = express.Router();
 
+const dbUrl = new URL(process.env.DATABASE_URL);
 
 const pool = new Pool({
-  user: 'capstone',
-  host: 'localhost',
-  database: 'capstone',
-  password: 'admin',
-  port: 5432,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  host: dbUrl.hostname,
+  port: dbUrl.port,
+  database: dbUrl.pathname.split('/')[1], // Ambil nama database dari path
+  ssl: {
+    rejectUnauthorized: false // Untuk development saja, sesuaikan untuk production
+  }
 });
 
-/* GET users listing. */
 router.get('/', async function(req, res, next) {
-  // Contoh route untuk mendapatkan data dari database
   try {
-    const { rows } = await pool.query('SELECT * FROM users');
+    const {rows} = await pool.query('SELECT * FROM users');
     res.json(rows);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Server error');
+    res.status(500).json({ error: 'Database error' });
   }
 });
+
+router.post('/', async function(req, res){
+  const { name, email } = req.body;
+
+  res.send({email,name});
+})
 
 module.exports = router;
